97f8a8b105a24d49b79e7485543a092cdb160756
zqz <2435454360@qq.com>
2021-07-06 13:40:29 +0800

    修改

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,12 @@ Route::prefix('admin')->namespace('Login')->group(function () {
     Route::post('register', 'AdminController@register');//管理员注册
     Route::post('logout', 'AdminController@logout');//注销
 });//cwp
+
+Route::prefix('hld')->namespace('AllTest')->group(function () {
+    Route::get('hldpeople', 'HldController@getHldPeople');//查询霍兰德测试中的不同类型人数分布
+    Route::get('hldage', 'HldController@getHldAge');//查询霍兰德测试中的不同类型中不同年龄分布
+    Route::get('hldgender', 'HldController@getHldGender');//查询霍兰德测试中不同类型不同类型分布
+    Route::get('testinfo','HldController@getTestInfo');//查询霍兰德测试结果
+    Route::get('detailed','HldController@detailed');//查询用户详细资料
+    Route::get('hldselect','HldController@hldSelect');//通过用户姓名或手机号查询霍兰德测试结果
+});//zqz
